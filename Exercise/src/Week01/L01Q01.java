8e81de08956a5013658526b13f651861b5d0d22e
XPS\Badrul Anuar <askbard@gmail.com>
2021-10-15 16:07:15 +0800

    Test

---
+++ Exercise/src/Week01/L01Q01.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Week01;
+
+/**
+ *
+ * @author Badrul Anuar
+ * 
+ *  a) Make sure your project name is your matrix number. Please rename it.
+ *  b) Create new packages with WeekXX - (i.e XX is the lab number)
+ *  c) Make sure your new packages start with WeekXX 
+ *  d) Start your lab with with the following format LXXQYY - (XX is your lab session number and YY is the question number). alway start with 01 and continues with 02, 03, ...
+ *  e) Delete this sample packages.
+ *  f) Commit and push once completed
+ */
+public class L01Q01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+    }
+    
+}
