75240217489f6fbbea31f56db82762b261268781
BAD-SPECTRE\askba <askbard@gmail.com>
2021-10-18 11:10:40 +0800

    Finish Class

---
+++ Exercise/src/Week01/E01.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Week01;
+
+/**
+ *
+ * @author askba
+ */
+public class E01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World in Class");
+        System.out.println("Hello World in Class");
+        System.out.println("Hello World in Class");
+        System.out.println("Hello World in Class");
+              
+        
+    }
+    
+}
