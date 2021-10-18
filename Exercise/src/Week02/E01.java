33fc8189bba93339f7ecf9815dcaffe86b7c0871
BAD-SPECTRE\askba <askbard@gmail.com>
2021-10-18 17:12:38 +0800

    Finish Class

---
+++ Exercise/src/Week02/E01.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Week02;
+
+/**
+ *
+ * @author askba
+ */
+public class E01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int number01, number02;
+        number01 = 1;
+        number02 = 2;
+        
+        System.out.println("Number 01 + Number 02 = " + (number01+number02));
+        System.out.printf("%d + %d = %d", number01, number02, number01+number02);
+    }
+    
+}
