8862188728316a3c1ee4736f6f1c71f09c11de74
BAD-SPECTRE\askba <askbard@gmail.com>
2021-10-21 11:04:54 +0800

    Tutorial Week 01

---
+++ Exercise/src/Week01/E02.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Week01;
+
+/**
+ *
+ * @author askba
+ */
+public class E02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello world in my first class");
+        System.out.println("Thanks");
+    }
+    
+}
