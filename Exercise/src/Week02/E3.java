5aae4776d3e6799bb34c4dff257cbb8b9b2c70d7
BAD-SPECTRE\askba <askbard@gmail.com>
2021-10-25 10:16:48 +0800

    First Example Week02

---
+++ Exercise/src/Week02/E3.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Week02;
+
+/**
+ *
+ * @author askba
+ */
+public class E3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        float num01, num02, num03;
+        num01 = 20;
+        num02 = 3;
+        num03 = num01/num02;
+        
+        System.out.printf("\nThe answer is %3.3f",num03);
+        
+        int numX01, numX02;
+        
+        double numX03;
+        numX01 = 20;
+        numX02 = 3;
+        numX03 = numX01/(float) numX02;
+        
+        System.out.printf("\nThe answer is %3.3f",numX03);
+        
+        
+    }
+    
+}
