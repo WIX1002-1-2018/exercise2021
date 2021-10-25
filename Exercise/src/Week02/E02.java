09aaec529a29e894de88a4a4b42b7e8fb32d2d15
BAD-SPECTRE\askba <askbard@gmail.com>
2021-10-25 10:05:58 +0800

    First Example Week02

---
+++ Exercise/src/Week02/E02.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Week02;
+
+/**
+ *
+ * @author askba
+ */
+public class E02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int number01, number02;
+        String myNameinMalay;
+        
+        number01 = 10;
+        number02 = 30;
+        System.out.println(number01+number02);
+        System.out.println(number01 + "+" + number02 + "=" + (number01+number02));
+        
+        myNameinMalay = "Nor Badrul Anuar";
+        System.out.println(myNameinMalay);
+        
+        System.out.print(number01 + "" + number02 + "\n");
+        System.out.println(number02);
+        
+        System.out.printf("%d + %d = %d", number01, number02,number01+number02);
+                
+        
+    }
+    
+}
