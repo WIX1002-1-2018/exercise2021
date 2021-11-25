0073a036a92482ac9b6591564c9a7325aed5321d
BAD-SPECTRE\askba <askbard@gmail.com>
2021-11-25 10:24:21 +0800

    week 06

---
+++ Exercise/src/Week06/W06T01.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Week06;
+
+/**
+ *
+ * @author askba
+ */
+public class W06T01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        char a = 'A';
+        char aDecimal = 65;
+        char aHexa = 0x41;
+        System.out.println(a);
+        System.out.println(aDecimal);
+        System.out.println(aHexa);
+        
+        if(a==65)
+            System.out.println("Yes");
+        
+        System.out.println(1+2*3)
+                ;
+        /**int i=0;
+        while(++i<10){
+            System.out.println(i);
+           if(i==10)
+              System.out.println("Program End");
+        }**/
+    }
+    
+}
