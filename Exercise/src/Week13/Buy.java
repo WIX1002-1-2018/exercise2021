09757228fcd19f41546237a42448d9b30515a0fd
Badrul <Badrul@host.docker.internal>
2022-01-21 10:26:45 +0800

    Week 13

---
+++ Exercise/src/Week13/Buy.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Week13;
+
+/**
+ *
+ * @author Badrul
+ */
+public class Buy {
+    private String name;
+    private double [] cost;
+    private double discout;
+
+    public Buy(String name, double [] cost, double discout) {
+        
+        this.name = name;
+        this.cost = cost;
+        this.discout = discout;
+    }
+    
+}
---
+++ Exercise/src/Week13/TestBuy.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Week13;
+
+/**
+ *
+ * @author Badrul
+ */
+public class TestBuy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double [] cost = {12.1,13.1};
+        
+        Buy a = new Buy("Ali",cost,30.0);
+        Buy a1 = new Buy("Abu",cost,30.0);
+    }
+    
+}
